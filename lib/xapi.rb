aaf2b45f1ede20e6a6c4520eef0b72b9e63b697d
Mohamad Abras <mohamad.abras@gmail.com>
2025-02-23 18:53:09 +0400

    simplify duration assignment in result initialization [skip ci]

--- lib/xapi.rb
+++ lib/xapi.rb
@@ -107,8 +107,7 @@ module Xapi
     elsif opts[:score_details].present?
       score = Score.new(raw: opts[:score_details][:raw], min: opts[:score_details][:min], max: opts[:score_details][:max])
     end
-    duration = opts[:duration].present? ?  opts[:duration] : nil
-    result = Result.new(duration: duration, score: score)
+    result = Result.new(duration: opts[:duration].presence, score: score)
     result.response = opts[:response] if opts[:response].present?
     result.success = opts[:success] if opts[:success].present?
     result.completion = opts[:completion] if opts[:completion].present?
